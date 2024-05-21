3401768e4d426c5f5d2d9f07519515e4aaa7aa44
EEDaryl <ejohdaryl@gmail.com>
2024-05-21 09:13:59 +0100

    Last changes

--- Section A/main.cpp
+++ Section A/main.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
 #include<vector>
 #include "Cards.h"
-#include "Cards.cpp"
+using namespace std;
 
-
-char heart = 3, diamond = 4, clubs = 5, spades = 6;
-Card card = Card(1, heart);
+char convertT(string type) {
+    if(type == "heart") {
+        return 3;
+    }
+    else if (type == "diamond") {
+        return 4;
+    }
+    else if (type == "clubs") {
+        return 5;
+    } 
+    else if (type == "spades") {
+        return 6;
+    } 
+}
+int num; 
+string type;
 
 int main() {
+    cout <<"Enter the card number";
+    cin>>num;
+    cout <<endl<<"Enter the card type: ";
+    cin >>type;
+    Card card = Card(num, convertT(type));
     card.Display();
     return 0;
 }
